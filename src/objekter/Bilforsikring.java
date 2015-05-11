6e232e0d323aae7c65a265ea0e51f5fd143c4309
4bZurd <s236612@stud.hioa.no>
2015-05-11 11:07:05 +0200

    KALKULATORER FFS!

--- src/objekter/Bilforsikring.java
+++ src/objekter/Bilforsikring.java
@@ -27,14 +27,11 @@ public class Bilforsikring extends Kjoretoyforsikring
     private int bilensVerdi;
     //---test for på beregnPris
     private int belopet;
-    private int innevarendeAr = Calendar.getInstance().get(Calendar.YEAR);
     private int arsModellen;
-    private int ar = innevarendeAr - arsModellen;
     private int hk;
     private int kmlengde;
     private int egenAndel;
     private final boolean garasjen;
-    private int fAlder;
     
     public Bilforsikring(  Kunde k, int e_andel, String registreringsnummer, int belop,
                            String fabrikant, String modell, String type, int hestekrefter, 
@@ -53,12 +50,10 @@ public class Bilforsikring extends Kjoretoyforsikring
         kmst = kilometerstand;
         //--test for beregnPris
         belopet = belop;
-        // ar = arsmodell;
         bonusen = bonus;
         hk = hestekrefter;
         kmlengde = km;
         garasjen = garasje;
-        //fAlder = foreralder;
         egenAndel = e_andel;
         arsModellen = arsmodell;
     }
@@ -303,238 +298,25 @@ public class Bilforsikring extends Kjoretoyforsikring
     {
         this.garasje = bekreftelse;
     }
-    
-    /**
-     * Denne metoden mottar kunde som parameter og gjør prisberegninger 
-     * utifra kundeinformasjon.
-     * @param kunde 
-     */
-    
-    @Override
-    public void beregnPris()
-    {
-        int bpTakst = 0;
-        double bpBilAlder = 0;
-        double bpKjorelengde = 0;
-        double bpHk = 0;
-        double bpGarasje = 0;
-        double bpEgenandel = 0;
-        double bpForerAlder = 0;
-        double bpEsp = 0;
-        double bpAlarm = 0;
-        double bpGps = 0;
-        double bpTilbud = 0;
-        
-        //Henter faktor for bilens grunnbeløp.
-        if (belopet > 0 && belopet <= 50000) 
-        {
-            bpTakst = 3500;
-        }
-        else if (belopet > 50000 && belopet <= 100000)
-        {
-            bpTakst = 4000;
-        }
-        else if (belopet > 100000 && belopet <= 150000)
-        {
-            bpTakst = 4500;
-        }
-        else if (belopet > 150000 && belopet <= 200000)
-        {
-            bpTakst = 5000;
-        }
-        else if (belopet > 200000 && belopet <= 250000)
-        {
-            bpTakst = 5500;
-        }
-        else if (belopet > 250000 && belopet <= 300000)
-        {
-            bpTakst = 6000;
-        }
-        else if (belopet > 300000 && belopet <= 400000)
-        {
-            bpTakst = 7000;
-        }
-        else if (belopet > 400000 && belopet <= 500000)
-        {
-            bpTakst = 8000;
-        }
-        else if (belopet > 500000 && belopet <= 600000)
-        {
-            bpTakst = 9000;
-        }
-        else if (belopet > 600000 && belopet <= 600000)
-        {
-            bpTakst = 10500;
-        }
-        else if (belopet > 750000 && belopet <= 1000000)
-        {
-            bpTakst = 13000;
-        }
-        else if (belopet > 1000000 && belopet <= 1500000)
-        {
-            bpTakst = 18000;
-        }
-        else if (belopet > 1500000 && belopet <= 2000000)
-        {
-            bpTakst = 23000;
-        }
-        else if (belopet > 2000000)
-        {
-            bpTakst = 33000;
-        }
-        
-        // Henter faktor for bilens alder.
-        if (ar > 0 && ar <= 8)
-        {
-            bpBilAlder = 0.2;
-        }
-        else if (ar > 8 && ar <= 16)
-        {
-            bpBilAlder = 0.3;
-        }
-        else if (ar > 16)
-        {
-            bpBilAlder = 0.5;
-        }
-        
-        // Henter faktor for forventet kjørelengde.
-        if (kmlengde == 8000)
-        {
-            bpKjorelengde = 0.2;
-        }
-        else if (kmlengde == 12000)
-        {
-            bpKjorelengde = 0.3;
-        }
-        else if (kmlengde == 16000)
-        {
-            bpKjorelengde = 0.4;
-        }
-        else if (kmlengde == 20000)
-        {
-            bpKjorelengde = 0.5;
-        }
-        else if (kmlengde == 25000)
-        {
-            bpKjorelengde = 0.6;
-        }
-        else if (kmlengde == 30000)
-        {
-            bpKjorelengde = 0.7;
-        }
-        
-        // Henter faktor for bilens hestekrefter.
-        if (hk > 0 && hk <= 100)
-        {
-            bpHk = 0.2;
-        }
-        else if (hk > 100 && hk <= 200)
-        {
-            bpHk = 0.4;
-        }
-        else if (hk > 200 && hk <= 300)
-        {
-            bpHk = 0.6;
-        }
-        else if (hk > 300 && hk <= 400)
-        {
-            bpHk = 0.8;
-        }
-        else if (hk > 400 && hk <= 500)
-        {
-            bpHk = 1.0;
-        }
-        else if (hk > 500)
-        {
-            bpHk = 1.2;
-        }
-        
-        //Henter faktor for garasje.
-        if (garasje == true)
-        {
-            bpGarasje = 0.0;
-        }
-        else
-        {
-            bpGarasje = 0.2;
-        }
-        
-        //Henter faktor for egenandel.
-        if (egenAndel == 2000)
-        {
-            bpEgenandel = 1.0;
-        }
-        else if (egenAndel == 4000)
-        {
-            bpEgenandel = 0.85;
-        }
-        else if (egenAndel == 8000)
-        {
-            bpEgenandel = 0.7;
-        }
-        else if (egenAndel == 12000)
-        {
-            bpEgenandel = 0.55;
-        }
-        else if (egenAndel == 16000)
-        {
-            bpEgenandel = 0.4;
-        }
-        else if (egenAndel == 20000)
-        {
-            bpEgenandel = 0.25;
-        }
-        else if (egenAndel == 30000)
-        {
-            bpEgenandel = 0.1;
-        }
-        
-        // Henter faktor for førers alder. 
-        if (fAlder >= 18 && fAlder <= 23)
-        {
-            bpForerAlder = 0.6;
-        }
-        else if (fAlder > 23 && fAlder <= 30)
-        {
-            bpForerAlder = 0.3;
-        }
-        else if (fAlder > 30)
-        {
-            bpForerAlder = 0.1;
-        }
-        
-        // Henter faktor for om bilen har Antiskrens/ESP.
-        if (esp == true)
-        {
-            bpEsp = 0.0;
-        }
-        else
-        {
-            bpEsp = 0.2;
-        }
-        
-        // Henter faktor for om bilen har godkjent alarm. 
-        if (alarmen == true)
-        {
-            bpAlarm = 0.0;
-        }
-        else
-        {
-            bpAlarm = 0.2;
-        }
-        
-        // Henter faktor for om bilen har GPS tracking. 
-        if (gjenkjenningen == true)
-        {
-            bpGps = 0.0;
-        }
-        else
-        {
-            bpGps = 0.2;
-        }
-        
-        bpTilbud = bpTakst*(bpBilAlder+bpKjorelengde+bpHk+bpGarasje+bpEgenandel+bpForerAlder+bpEsp+bpAlarm+bpGps);
-        System.out.println("FAEN");
+
+    public int getBelopet() {
+        return belopet;
+    }
+
+    public int getArsModell() {
+        return arsModellen;
+    }
+
+    public int getHk() {
+        return hk;
+    }
+
+    public int getKmlengde() {
+        return kmlengde;
+    }
+
+    public int getEgenAndel() {
+        return egenAndel;
     }
     
     @Override
--- src/objekter/Reiseforsikring.java
+++ src/objekter/Reiseforsikring.java
@@ -11,16 +11,14 @@ package objekter;
 
 public class Reiseforsikring extends Personligforsikring
 {
-    private String sone;
+    private int sone;
     private int belopet;
     private double soneBeregn;
     private double forsorgerpris = 1;
     private double premie;
     private double sum;
-    
-    
-    
-    public Reiseforsikring(Kunde k, int e_andel, boolean fs, int antallBarn, String son, int belop )
+        
+    public Reiseforsikring(Kunde k, int e_andel, boolean fs, int antallBarn, int son, int belop )
     {
         
         super(k, e_andel, fs, antallBarn);
@@ -28,24 +26,14 @@ public class Reiseforsikring extends Personligforsikring
         belopet = belop;
     }
 
-    public String getSone()
+    public int getSone()
     {
         return sone;
     }
 
-    public void setSone(String s)
+    public void setSone(int s)
     {
         sone = s;
-    }
-    
-    public int getBelop()
-    {
-        return belopet;
-    }
-    
-    public void setBelop(int b)
-    {
-        belopet = b;
     }
 
     public double getSoneBeregn()
@@ -77,6 +65,10 @@ public class Reiseforsikring extends Personligforsikring
     {
         premie = p;
     }
+
+    public int getBelopet() {
+        return belopet;
+    }
     
     @Override
     public void beregnPris()
@@ -88,7 +80,7 @@ public class Reiseforsikring extends Personligforsikring
         }
             
             
-        /*switch (sone)
+        switch (sone)
         {
             case 1: soneBeregn = 1;//Hva man skal gange prisen med
                     break;
@@ -96,7 +88,7 @@ public class Reiseforsikring extends Personligforsikring
                     break;
             case 3: soneBeregn = 1.20;//Hva man skal gange prisen med
                     break;
-        }*/
+        }
         premie = belopet*0.20*soneBeregn*forsorgerpris;
     }
     
