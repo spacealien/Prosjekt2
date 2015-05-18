c72327f63a469ce9e3fbc51b87e21de193f9bc2d
spacealien <odd.hoel@hotmail.com>
2015-05-18 12:03:20 +0200

    Merge origin/master
    Conflicts:
    	src/register/HovedRegister.java

--- src/gui/LoginVindu.java
+++ src/gui/LoginVindu.java
@@ -1,19 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Programmets login vindu.
+ *
  */
+
 package gui;
 
-/**
- *
- * @author Odd
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
